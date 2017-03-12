93c4adeacf1d73d26e71fecd531afbdf53248fe0
David Steed <davidasteed@gmail.com>
2017-03-11 20:14:14 -0500

    improved html element appearance for success\/failure conditions and also clean out the test login function and removed authId from form POST

--- src/html/js/newthought.js
+++ src/html/js/newthought.js
@@ -6,36 +6,6 @@
   // new thoughts POST url
   let postURL = 'https://thoughter.herokuapp.com/api/Thoughts';
 
-  let loginInfo = {
-    'username': 'jordan',
-    'password': 'foobar'
-  }
-
-  // store the login url NOTE test code
-  let loginUrl = 'https://thoughter.herokuapp.com/api/Authors/login';
-
-  // store the authorId NOTE test code
-  let authorId;
-
-  // test login to obtain info NOTE test code
-  let loginToken; // store the token
-
-  window.nameSpace.testLogin = function testLogin(input) {
-    fetch(loginUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(input)
-    })
-    .then(function handleResponse(response) {
-      response.json().then(function handleJSON(jsonObj){
-        loginToken = jsonObj.id;
-        authorId = jsonObj.userId;
-      });
-    });
-  };
-
   // function to submit a new thought
   window.nameSpace.newThoughtSubmit = function newThoughtSubmit(bodyObj) {
     fetch(postURL, {
@@ -49,13 +19,12 @@
         $('.btn-success').toggleClass('formSuccess');
       }
       else {
-        $('.btn-danger').toggleClass('formSuccess');
+        let postFail = "Submission failed!";
+        $('.btn-danger').toggleClass('formSuccess')
+          .text(postFail);
       }
     });
   };
-
-  //variable to store login response TODO:  get rid of this variable
-  let response = {};
 
   // variable to store the user "content" for the new thought
   let content;
@@ -66,15 +35,14 @@
     if ($('input')[0].value) {
       content = $('input')[0].value;
     } else {
-      console.log("Please enter something");
+      let failText = "Please enter something!";
+      $('.btn-danger').toggleClass('formSuccess')
+        .text(failText);
       return;
     }
 
-    // attempt to login NOTE test code
-    window.nameSpace.testLogin(loginInfo);
-
     // build body content
-    let postBody = { content, authorId };
+    let postBody = { content };
 
     // attempt to post the new thought, passing in the body content
     window.nameSpace.newThoughtSubmit(postBody);
