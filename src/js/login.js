12312620fb9d5506045ab4e0dd8d92f04b439f0f
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2017-03-13 15:28:20 -0400

    docblock added

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,12 @@
 (function() {
   'use strict';
   window.nameSpace = window.nameSpace || {};
+  * [This function fetches login data from the api]
+* @param  {[url]} https   [api url]
+* @param  {[json]} headers [content type of the data's header]
+* @param  {[json-stringify]} body    [turns the body into stringified JSON data]
+* @return {[promise]}         [handles response objects status]
+*/
 
   window.loginInput.submitLogin = function loginInput('https://thoughter.herokuapp.com/api/Authors/login', userData) {
 
